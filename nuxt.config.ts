e694403807f52d741dbdfc45528b4edb3d23f012
MinhPhuPham <phamminhphueur@gmail.com>
2022-09-29 16:47:52 +0700

    Initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    head: {
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'description', name: 'description', content: '' },
+          { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [
+          // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ],
+      },
+      alias: {
+        '~/*': './*',
+      },
+      buildModules: [
+        '@nuxtjs/composition-api/module'
+      ]
+})
